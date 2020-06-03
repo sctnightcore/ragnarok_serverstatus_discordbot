ce83201849333f77b6810bfb7aed2ed4b6cac4fa
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2020-06-03 17:40:12 +0700

    use loop for get post data / add socket timeout

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ class Main(Config, Log):
     def __init__(self):
         self.read_config("config.json")
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(0.5)
 
     def check_socket(self, ip, port):
         result = self.socket.connect_ex((ip, port))
@@ -37,23 +38,17 @@ class Main(Config, Log):
         embed.set_footer(text='by sctnightcore')
 
         embed.set_timestamp()
-        embed.add_embed_field(name='Login Server', 
-            value= self.config['true-icon'] if post_data['Login'] else self.config['false-icon']
-        )
-        embed.add_embed_field(name='Char Server',
-            value= self.config['true-icon'] if post_data['Char'] else self.config['false-icon']
-        )
-        embed.add_embed_field(name='Map Server',
-            value= self.config['true-icon'] if post_data['Map'] else self.config['false-icon']
-        )
-
+        for i in post_data:
+            embed.add_embed_field(name="{} - {}".format(i['Name'], i['Port']),
+                value=self.config['true-icon'] if i['Name'] else self.config['false-icon']
+            )
+    
         webhook.add_embed(embed)
         response = webhook.execute()
         self.message(response)
         self.message(post_data)
         assert response != 204
-
-        
+    
     def main_loop(self):
         while True:
             data = {}
